61fec61ad8bbb61fd81ca1fa0e3092bc31f2036a
alex-2719 <alex.sbox24@gmail.com>
2020-03-03 14:48:00 +0300

    dream-team task complete

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,17 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (Array.isArray(members)){
+    let result =  members.reduce(function(prev,item){
+      if (typeof(item) ==='string'){
+        let trimStr = item.trim();
+        return prev+trimStr[0].toUpperCase();
+      }
+      else {
+        return prev;
+      }
+    },'');
+    return result === '' ? false : result.split('').sort().join('');
+  }
+  else {
+    return false;
+  }
 };
