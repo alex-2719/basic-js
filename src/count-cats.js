0a8229ad043336bdc397e672ebbd3b746b06e253
alex-2719 <alex.sbox24@gmail.com>
2020-03-03 14:01:50 +0300

    count-cat task complete

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,4 +1,20 @@
-module.exports = function countCats(/* matrix */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function countCats( matrix ) {
+  if (Array.isArray(matrix)){
+    if (matrix.length !=0){
+      return matrix.reduce(function(prev, item){
+          if (Array.isArray(item)){
+            return  prev + countCats(item);
+          }
+          else {
+            return (item === '^^' && item.length === 2) ? prev+1 : prev;
+          }
+        },0);
+    }
+    else {
+      return 0;
+    } 
+  }
+  else {
+    return 0;
+  }
 };
