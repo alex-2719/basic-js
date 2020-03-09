8c9f358ea5d35fa7a2318afbb341841864a79fa8
alex <alex.sbox24@gmail.com>
2020-03-09 23:20:16 +0300

    extended-repeater task complete

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,24 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+let checkStr = (strValue, separator) =>{return typeof(strValue) !== 'undefined' ? strValue : separator};
+
+module.exports = function repeater( str, options ) {
+    str = "" + str;
+    let repeatTimes = options.repeatTimes || 0;
+    let separator = "" + checkStr(options.separator, '+');
+    let additionRepeatTimes = options.additionRepeatTimes || 0;
+    let additionSeparator = "" + checkStr(options.additionSeparator, "|");
+    let addition = "" + checkStr(options.addition,"");
+    let result = [];
+    for (i = 0; i < repeatTimes ; i++){
+        let additionalArr = [];
+        for(j = 0 ; j < additionRepeatTimes ; j++){
+            additionalArr.push(addition);
+        }
+        if (additionalArr.length == 0 && addition != ""){
+            additionalArr.push(addition);
+        }
+
+        result.push (str + additionalArr.join(additionSeparator));
+    }
+    return result.length != 0 ? result.join(separator) : str + (addition != "" ? addition : "");
+
 };
-  
