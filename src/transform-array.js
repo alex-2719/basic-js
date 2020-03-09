c4cc4ac7944dbfabbaa538e50507ec9220fa37bd
sam <sam@localhost>
2020-03-09 16:40:20 +0300

    transform-array task complete

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,30 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function transform( arr ) {
+    if (Array.isArray(arr)){
+        let array = new Array();
+        arr.forEach(function(item, index){
+            if (item === "--discard-next"){
+                if ((arr.length -1) > index){
+                    arr.splice(index,1);
+                }
+            } else if (item === "--discard-prev"){
+                if (index > 0 ){
+                    array.pop();
+                }
+            } else if (item === "--double-next"){
+                if ((arr.length -1) > index){
+                    array.push(arr[ index + 1]);
+                }
+            } else if (item === "--double-prev"){
+                if (index > 0 ){
+                    array.push(arr[index -1]);
+                }
+            }else {
+                array.push(item);
+            }
+        });
+        return array;
+    }
+    else {
+        throw new Error();
+    }
 };
